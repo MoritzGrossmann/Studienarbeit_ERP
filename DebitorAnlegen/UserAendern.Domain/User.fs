cd0643ca090fb98b199f7315097d216edb35a54d
Moritz Großmann <moritz.grossmann@googlemail.com>
2017-10-10 11:56:05 +0200

    Projekte Angelegt

---
+++ DebitorAnlegen/UserAendern.Domain/User.fs
@@ -0,0 +1,15 @@
+﻿namespace UserAendern.Domain
+
+type User = {
+    Vorname : string
+    Nachname : string
+}
+
+
+
+type ISaveUser =
+    abstract CreateUser : user : User -> bool
+    abstract UpdateUser : user : User -> bool
+
+type IDeleteUser =
+    abstract DeleteUser : user : User -> bool
