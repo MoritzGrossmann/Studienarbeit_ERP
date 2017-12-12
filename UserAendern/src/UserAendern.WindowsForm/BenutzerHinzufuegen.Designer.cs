ced7928bd2dbb1555cd400e97565950ac4a8b801
Moritz Großmann <moritz.grossmann95@googlemail.com>
2017-12-12 09:23:15 +0100

    rechtschreibfehler korrigiert

--- UserAendern/src/UserAendern.WindowsForm/BenutzerHinzufuegen.Designer.cs
+++ UserAendern/src/UserAendern.WindowsForm/BenutzerHinzufuegen.Designer.cs
@@ -46,9 +46,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "username";
+            this.label1.Text = "Username";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btn_ok
